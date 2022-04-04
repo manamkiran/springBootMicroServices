29ea148dc40c6e8fe75851c9032815b68e05ff34
Kiran Manam <manamkiranyadav@gmail.com>
2022-04-04 14:29:27 +0530

    Adding WebClient inplace of RestTemplate

--- movie-catalog-service/src/main/java/manam/kiran/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/manam/kiran/resources/MovieCatalogResources.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import manam.kiran.models.CatalogItem;
 import manam.kiran.models.Movie;
@@ -17,9 +19,12 @@ import manam.kiran.models.Rating;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResources {
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
+
+	@Autowired
+	private WebClient.Builder webClientBuilder;
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -28,8 +33,22 @@ public class MovieCatalogResources {
 
 		return ratings.stream().map(rating -> {
 
-			Movie movie= restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-			return new CatalogItem(rating.getMovieId(), rating.getMovieId()+" Description", rating.getRating());
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(rating.getMovieId(), rating.getMovieId() + " Description", rating.getRating());
+		}).collect(Collectors.toList());
+
+	}
+
+	@RequestMapping("/web/{userId}")
+	public List<CatalogItem> getCatalogViaWebClient(@PathVariable("userId") String userId) {
+
+		List<Rating> ratings = Arrays.asList(new Rating("RRR", 4), new Rating("Radhe Shyam", 2));
+
+		return ratings.stream().map(rating -> {
+
+			Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/" + rating.getMovieId()).retrieve()
+					.bodyToMono(Movie.class).block();
+			return new CatalogItem(rating.getMovieId(), rating.getMovieId() + " Description", rating.getRating());
 		}).collect(Collectors.toList());
 
 	}
