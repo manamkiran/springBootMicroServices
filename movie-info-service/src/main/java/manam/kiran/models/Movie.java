9ac993581bf2dd5fd831116daf281c2f5355454c
Kiran Manam <manamkiranyadav@gmail.com>
2022-04-05 15:49:31 +0530

    Added code to call external API to get movie Info and added relavant fields to Movie class

--- movie-info-service/src/main/java/manam/kiran/models/Movie.java
+++ movie-info-service/src/main/java/manam/kiran/models/Movie.java
@@ -2,30 +2,53 @@ package manam.kiran.models;
 
 public class Movie {
 
-	private String movieId;
-	private String name;
+	private String id;
+	private String title;
+	private String overview;
+	private String original_title;
+
+	public String getOriginal_title() {
+		return original_title;
+	}
+
+	public void setOriginal_title(String original_title) {
+		this.original_title = original_title;
+	}
+
+	public Movie() {
+
+	}
+
+	public Movie(String id, String title, String overview) {
+		this.id = id;
+		this.title = title;
+		this.overview = overview;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getOverview() {
+		return overview;
+	}
+
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
 
 	
 	
-	public Movie(String movieId, String name) {
-		this.movieId = movieId;
-		this.name = name;
-	}
-
-	public String getMovieId() {
-		return movieId;
-	}
-
-	public void setMovieId(String movieId) {
-		this.movieId = movieId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 }
--- movie-info-service/src/main/java/manam/kiran/resources/MovieResource.java
+++ movie-info-service/src/main/java/manam/kiran/resources/MovieResource.java
@@ -1,18 +1,28 @@
 package manam.kiran.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import manam.kiran.models.Movie;
 
 @RestController
 @RequestMapping("movies")
 public class MovieResource {
+	
+	@Value("${api.key}")
+	private String apiKey;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable("movieId")String movieId) {
-		return new Movie(movieId, "Test Movie");
+		Movie movie = restTemplate.getForObject("https://api.themoviedb.org/3/movie/"+ movieId+"?api_key="+apiKey, Movie.class);
+		return movie;
 		
 	}
 }
