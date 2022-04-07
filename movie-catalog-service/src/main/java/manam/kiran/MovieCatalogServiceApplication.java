a6023c78574f597f26c37c1ba84a25741a378fcd
Kiran Manam <manamkiranyadav@gmail.com>
2022-04-07 18:40:45 +0530

    Added Resilience4j. It needs AOP to work so added the same

--- movie-catalog-service/src/main/java/manam/kiran/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/manam/kiran/MovieCatalogServiceApplication.java
@@ -1,5 +1,7 @@
 package manam.kiran;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -9,6 +11,9 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType;
+import io.github.resilience4j.common.circuitbreaker.configuration.CircuitBreakerConfigCustomizer;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class MovieCatalogServiceApplication {
@@ -17,10 +22,23 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		HttpComponentsClientHttpRequestFactory client = new HttpComponentsClientHttpRequestFactory();
+		/*HttpComponentsClientHttpRequestFactory client = new HttpComponentsClientHttpRequestFactory();
 		client.setConnectTimeout(3000);
-		return new RestTemplate(client);
+		return new RestTemplate(client);*/
+		return new RestTemplate();
 	}
+	
+	/*@Bean
+    public CircuitBreakerConfigCustomizer externalServiceFooCircuitBreakerConfig() {
+        return CircuitBreakerConfigCustomizer
+                .of("catalogService",
+                        builder -> builder.slidingWindowSize(10)
+                                .slidingWindowType(SlidingWindowType.COUNT_BASED)
+                                .waitDurationInOpenState(Duration.ofSeconds(5))
+                                .minimumNumberOfCalls(5)
+                                .recordExceptions(IllegalStateException.class)
+                                .failureRateThreshold(50.0f));
+    }*/
 	
 	@Bean
 	public WebClient.Builder getWebClient() {
--- movie-catalog-service/src/main/java/manam/kiran/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/manam/kiran/resources/MovieCatalogResources.java
@@ -1,17 +1,19 @@
 package manam.kiran.resources;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import manam.kiran.models.CatalogItem;
 import manam.kiran.models.Movie;
 import manam.kiran.models.Rating;
@@ -28,17 +30,27 @@ public class MovieCatalogResources {
 	private WebClient.Builder webClientBuilder;
 
 	@RequestMapping("/{userId}")
+	@CircuitBreaker(name = "catalogService", fallbackMethod = "getFallBackCatalog")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
+		
 		List<Rating> ratings = restTemplate
 				.getForObject("http://rating-data-service/ratingdata/users/" + userId, UserRating.class).getRatings();
 
 		return ratings.stream().map(rating -> {
 
-			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(),
+					Movie.class);
 			return new CatalogItem(movie.getTitle(), movie.getOverview(), rating.getRating());
 		}).collect(Collectors.toList());
 
+	}
+
+	public List<CatalogItem> getFallBackCatalog(@PathVariable("userId") String userId, IllegalStateException il) {
+		return Arrays.asList(new CatalogItem("No Movie", il.getLocalizedMessage(), 0));
+	}
+
+	public List<CatalogItem> getFallBackCatalog(@PathVariable("userId") String userId) {
+		return Arrays.asList(new CatalogItem("No Movie", "", 0));
 	}
 
 	@RequestMapping("/web/{userId}")
